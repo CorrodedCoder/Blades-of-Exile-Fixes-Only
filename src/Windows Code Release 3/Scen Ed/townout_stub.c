a08c02508f326ce322c4db33533026d22cc8b6cb
CorrodedCoder <38778644+CorrodedCoder@users.noreply.github.com>
2023-12-03 00:48:49 +0000

    Temporarily replace townout.c with townout_stub.c
    As is mentioned in the original docs, this file is actually the Mac version because the Windows version is lost, but it should be possible to rebuild this.
    In the meantime, replace it with stubbed out functions so that we can build it.

---
+++ src/Windows Code Release 3/Scen Ed/townout_stub.c
@@ -0,0 +1,198 @@
+#include <Windows.h>
+#include "global.h"
+
+void init_town(short size)
+{
+
+}
+
+void init_out()
+{
+
+}
+
+Boolean new_town(short which_town)
+{
+	return FALSE;
+}
+
+void edit_placed_monst_event_filter(short item_hit)
+{
+
+}
+
+void edit_placed_monst_adv_event_filter(short item_hit)
+{
+}
+
+void edit_sign_event_filter (short item_hit)
+{
+}
+
+void edit_out_strs_event_filter(short item_hit)
+{
+}
+
+void edit_out_strs()
+{
+}
+
+void edit_town_strs_event_filter(short item_hit)
+{
+
+}
+
+void edit_town_strs()
+{
+
+}
+
+void pick_town_num_event_filter(short item_hit)
+{
+
+}
+
+void change_ter_event_filter(short item_hit)
+{
+}
+
+void edit_out_wand_event_filter(short item_hit)
+{
+}
+
+void edit_out_wand(short mode)
+{
+
+}
+
+
+void outdoor_details_event_filter(short value)
+{
+
+}
+
+void outdoor_details()
+{
+
+}
+
+void edit_town_details()
+{
+
+}
+
+void edit_town_details_event_filter(short value)
+{
+
+}
+
+void edit_town_events()
+{
+
+}
+
+void edit_town_events_event_filter(short value)
+{
+
+}
+
+void edit_advanced_town()
+{
+
+}
+
+void edit_advanced_town_event_filter(short value)
+{
+
+}
+
+void edit_basic_dlog_event_filter(short value)
+{
+
+}
+
+void edit_talk_node_event_filter(short value)
+{
+
+}
+
+void pick_out_event_filter(short value)
+{
+
+}
+
+void pick_import_town_event_filter(short value)
+{
+
+}
+
+short pick_import_town(short value_a, short value_b)
+{
+	return 0;
+}
+
+void new_town_event_filter(short value)
+{
+
+}
+
+void edit_placed_item_event_filter(short value)
+{
+
+}
+
+void delete_last_town()
+{
+
+}
+
+void edit_town_wand_event_filter(short value)
+{
+
+}
+
+void edit_town_wand()
+{
+
+}
+
+
+void edit_placed_monst(short value)
+{
+
+}
+
+void edit_sign(short value_a,short value_b)
+{
+
+}
+
+short pick_town_num(short value_a, short value_b)
+{
+	return 0;
+}
+
+void change_ter(short * ptr_a,short * ptr_b,short * ptr_c)
+{
+
+}
+
+void edit_basic_dlog(short value)
+{
+
+}
+
+void edit_talk_node(short value_a, short value_b)
+{
+
+}
+
+short pick_out(location value)
+{
+	return 0;
+}
+
+void edit_placed_item(short value)
+{
+
+}
